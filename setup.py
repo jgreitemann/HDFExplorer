7f6d619ce5894dddfbf8f9fe92c55fd634a29b37
Jonas Greitemann <j.greitemann@lmu.de>
2016-04-29 15:53:46 +0200

    Copied a minimal Gtk example program

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='HDFExplorer',
+      version='0.1',
+      description='Graphically browse HDF5 files',
+      author='Jonas Greitemann',
+      author_email='jgreitemann@gmail.com',
+      url='https://www.github.com/jgreitemann/HDFExplorer',
+      packages=['hdfexplorer'],
+      scripts=['h5explorer'],
+     )
