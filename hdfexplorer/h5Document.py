d32d0ce9bbb4d105b5ce7683ac9b44659b148728
Jonas Greitemann <j.greitemann@lmu.de>
2016-09-18 21:11:33 +0200

    Added ability to inspect HDF5 groups

--- hdfexplorer/h5Document.py
+++ hdfexplorer/h5Document.py
@@ -32,7 +32,11 @@ class h5Document(GObject.Object):
         self.dset_datatype_label = builder.get_object("dataset-datatype-label")
         self.dset_path_label = builder.get_object("dataset-path-label")
 
-        # set up attributes tree
+        self.group_name_label = builder.get_object("group-name-label")
+        self.group_children_label = builder.get_object("group-children-label")
+        self.group_path_label = builder.get_object("group-path-label")
+
+        # set up attributes trees
         self.dset_attributes_tree = builder.get_object("dataset-attributes-tree")
         col = Gtk.TreeViewColumn("Key")
         cell = Gtk.CellRendererText()
@@ -44,6 +48,18 @@ class h5Document(GObject.Object):
         col.pack_start(cell, False)
         col.add_attribute(cell, "text", 1)
         self.dset_attributes_tree.append_column(col)
+
+        self.group_attributes_tree = builder.get_object("group-attributes-tree")
+        col = Gtk.TreeViewColumn("Key")
+        cell = Gtk.CellRendererText()
+        col.pack_start(cell, False)
+        col.add_attribute(cell, "text", 0)
+        self.group_attributes_tree.append_column(col)
+        col = Gtk.TreeViewColumn("Value")
+        cell = Gtk.CellRendererText()
+        col.pack_start(cell, False)
+        col.add_attribute(cell, "text", 1)
+        self.group_attributes_tree.append_column(col)
 
         # set up data tree
         self.dset_tree = builder.get_object("dataset-tree")
@@ -94,51 +110,61 @@ class h5Document(GObject.Object):
             self.stack.set_visible_child_name("placeholder")
             return
         h5_path, h5_object = model.get_h5_object(iter)
-        if not isinstance(h5_object, h5py.Dataset):
-            self.stack.set_visible_child_name("placeholder")
-            return
-        # dataset has been selected
+        if isinstance(h5_object, h5py.Dataset):
+            # retrieve existing model or create a new one
+            if not h5_path in self.entities:
+                self.entities[h5_path] = h5DatasetModel(h5_object)
 
-        # retrieve existing model or create a new one
-        if not h5_path in self.entities:
-            self.entities[h5_path] = h5DatasetModel(h5_object)
+            # Overview tab
+            self.dset_name_label.set_label(basename(h5_object.name))
+            self.dset_path_label.set_label(h5_object.name)
+            self.dset_shape_label.set_label(str_shape(h5_object.shape))
+            self.dset_datatype_label.set_label(str(h5_object.dtype))
 
-        # Overview tab
-        self.dset_name_label.set_label(basename(h5_object.name))
-        self.dset_path_label.set_label(h5_object.name)
-        self.dset_shape_label.set_label(str_shape(h5_object.shape))
-        self.dset_datatype_label.set_label(str(h5_object.dtype))
+            # Attributes tab
+            self.dset_attributes_tree.set_model(h5AttributesModel(h5_object.attrs))
 
-        # Attributes tab
-        self.dset_attributes_tree.set_model(h5AttributesModel(h5_object.attrs))
-
-        # Data tab
-        # clear tree view columns & create new ones
-        for c in self.dset_tree.get_columns():
-            self.dset_tree.remove_column(c)
-        col = Gtk.TreeViewColumn("")
-        cell = Gtk.CellRendererText()
-        cell.set_property("foreground", "#979a9b")
-        cell.set_property("xalign", 1.0)
-        cell.set_property("font", "Cantarell Bold 11")
-        col.pack_start(cell, False)
-        col.add_attribute(cell, "text", 0)
-        col.set_sizing(Gtk.TreeViewColumnSizing.FIXED)
-        col.set_expand(True)
-        self.dset_tree.append_column(col)
-        for i in range(self.entities[h5_path].do_get_n_columns() - 1):
-            col = Gtk.TreeViewColumn(str(i))
+            # Data tab
+            # clear tree view columns & create new ones
+            for c in self.dset_tree.get_columns():
+                self.dset_tree.remove_column(c)
+            col = Gtk.TreeViewColumn("")
             cell = Gtk.CellRendererText()
-            cell.set_property("xalign", 0.0)
+            cell.set_property("foreground", "#979a9b")
+            cell.set_property("xalign", 1.0)
+            cell.set_property("font", "Cantarell Bold 11")
             col.pack_start(cell, False)
-            col.add_attribute(cell, "text", i+1)
+            col.add_attribute(cell, "text", 0)
             col.set_sizing(Gtk.TreeViewColumnSizing.FIXED)
             col.set_expand(True)
             self.dset_tree.append_column(col)
+            for i in range(self.entities[h5_path].do_get_n_columns() - 1):
+                col = Gtk.TreeViewColumn(str(i))
+                cell = Gtk.CellRendererText()
+                cell.set_property("xalign", 0.0)
+                col.pack_start(cell, False)
+                col.add_attribute(cell, "text", i+1)
+                col.set_sizing(Gtk.TreeViewColumnSizing.FIXED)
+                col.set_expand(True)
+                self.dset_tree.append_column(col)
 
-        # put changes in effect
-        self.dset_tree.set_model(self.entities[h5_path])
-        self.stack.set_visible_child_name("dataset-notebook")
+            # put changes in effect
+            self.dset_tree.set_model(self.entities[h5_path])
+            self.stack.set_visible_child_name("dataset-notebook")
+        elif isinstance(h5_object, h5py.Group):
+            # Overview tab
+            self.group_name_label.set_label(basename(h5_object.name))
+            self.group_path_label.set_label(h5_object.name)
+            self.group_children_label.set_label(str(len(h5_object)))
+
+            # Attributes tab
+            self.group_attributes_tree.set_model(
+                h5AttributesModel(h5_object.attrs))
+
+            # put changes in effect
+            self.stack.set_visible_child_name("group-notebook")
+        else:
+            self.stack.set_visible_child_name("placeholder")
 
     def on_close(self, window, event):
         self.app.remove_window(window)
